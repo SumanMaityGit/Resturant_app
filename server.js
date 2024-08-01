e3ae486a440a285585467a486962bb0b305cfcb4
unknown <sumanmaity7477@gmail.com>
2024-08-02 00:56:10 +0530

    final changes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const colors = require("colors");
 const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 
 // dot env configuration
 dotenv.config();
@@ -17,25 +17,20 @@ const app = express();
 // middlewares
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // route
 // URL => http://localhost:8000
-app.use("/api/v1/test", require("./routes/testRoutes"));
 app.use("/api/v1/auth", require("./routes/authRoutes"));
 app.use("/api/v1/user", require("./routes/userRoutes"));
 app.use("/api/v1/resturant", require("./routes/resturantRoutes"));
-app.use("/api/v1/category",require("./routes/categoryRoutes"));
-app.use("/api/v1/food",require("./routes/foodRoutes"));
-
-app.get("/", (req,res) => {
-    return res.status(200).send("<h1>Welcome to Food Server</h1>");
-});
+app.use("/api/v1/category", require("./routes/categoryRoutes"));
+app.use("/api/v1/food", require("./routes/foodRoutes"));
 
 // PORT
 const PORT = process.env.PORT || 5000;
 
 // Listen
 app.listen(PORT, () => {
-    console.log("Server running on ${PORT}".white.bgMagenta);
+  console.log("Server running on ${PORT}".white.bgMagenta);
 });
